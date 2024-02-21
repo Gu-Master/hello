fb3710bd8eef36caad5aaa1bd33a80148a9e7238
Gu-Master <61457laize@gmail.com>
2024-02-22 02:13:06 +0700

    1

---
+++ src/test/java/web/helloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package web.helloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
